b0b194240ef59f1685f244561db5409e42e2b48e
Frank Shubi Basheke <75610367+frankshubibasheke@users.noreply.github.com>
2022-09-27 04:33:59 +0300

    Create 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* set_string - function that sets the value of a pointer to a char.
+* @s: source for copy.
+* @to: string to copy to.
+*
+* Return: Nothing.
+*/
+
+void set_string(char **s, char *to)
+{
+        *s = to;
+}
+Footer
+© 2022 GitHub, Inc.
