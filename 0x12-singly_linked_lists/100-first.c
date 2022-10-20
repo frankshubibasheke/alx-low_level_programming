4b34e27870183f5ef41718c0c1365918cc7e917c
Frank Shubi Basheke <75610367+frankshubibasheke@users.noreply.github.com>
2022-10-19 22:31:33 -0700

    Create 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * premain - execute code before the main
+ * return - non return, void function
+ */
+void __attribute__ ((constructor)) premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+Footer
+© 2022 GitHub, Inc.
